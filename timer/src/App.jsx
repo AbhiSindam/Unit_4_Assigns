2b88bad06398e5e3acf77984f01b303de7cb22bd
AbhiSindam <abhisindam206@gmail.com>
2022-04-23 18:49:24 +0530

    .

---
+++ timer/src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { Timer } from './components/Timer';
+import { useState } from 'react';
+
+function App() {
+  const [value, setValue] = useState(0)
+
+  
+
+  // console.log("value", value)
+
+  return (
+    <div className="App">
+      <input type="number" name="" id="" onChange={(e) => setValue(e.target.value)} />
+      
+      <Timer data={value}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ timer/src/components/Timer.jsx
@@ -0,0 +1,37 @@
+import {useState, useEffect}  from 'react'
+
+
+export const Timer = ({data}) => {
+
+    data = +data
+    // console.log(data)
+
+    const [time, setTime] = useState(0)
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            setTime((p) => {
+                if (p === data){
+                    clearInterval(id)
+                    return 0
+                }
+                return p+1
+            });
+        }, 1000);
+        return () => {
+            clearInterval(id)
+        }    
+
+    },[data])
+
+
+
+
+
+
+    return(
+        <div>
+            <h1>Timer: {time}</h1>
+        </div>
+    )
+}
